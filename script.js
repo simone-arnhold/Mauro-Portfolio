9e97261642aa28940162ece812a69385d54f8b75
Simone Arnhold <simone.arnhold@outlook.com>
2023-06-20 15:30:13 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+menuButton = document.getElementById("selector")
+body = document.querySelector("body")
+
+menuButton.addEventListener("click", () => {
+    body.classList.toggle("menu-main-open");
+    console.log("aa")
+})
+
+// function menuSelect(element) {
+    // element.preventDefault();
+    // expandend = selector.attr("aria-expanded") == 'false' ? 'true' : 'false';
+    // selector.attr("aria-expanded", expandend);
+    // menu = $("#menu");
+    // expandend = menu.attr("aria-hidden") == 'false' ? 'true' : 'false';
+    // menu.attr("aria-hidden", expandend);
+// }
+
